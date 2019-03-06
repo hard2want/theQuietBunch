6eda1b205cafb9606b1568fc6425d4334f4bf467
hard2want <thedukeofchicago@gmail.com>
2019-03-05 20:39:53 -0600

    Initial Commit

---
+++ The Quiet Bunch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Quiet Bunch
+//
+//  Created by The Duke on 3/5/19.
+//  Copyright © 2019 The Duke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
